500e02348d0d8ee687c9e9664a913c44c14ae0a7
Baibhav Joshi <92478959+BaibhavJoshi@users.noreply.github.com>
2023-08-25 14:20:15 +0200

    Add files via upload
    > Completed script.js.
    > Completed styles.css

--- js/script.js
+++ js/script.js
@@ -1,13 +1,91 @@
-DEFAULT_SIZE = 14;
+const DEFAULT_SIZE = 14;
+const DEFAULT_MODE = "pencil";
+const DEFAULT_COLOR = "#09078d"
 
+let currentColor = DEFAULT_COLOR;
+let currentMode = DEFAULT_MODE;
+let currentSize = DEFAULT_SIZE;
+
+const colorPicker = document.getElementById("pencil-color");
+const boardSizeValue = document.getElementById("range-value");
+const sizeSlider = document.getElementById("input-range");
 const board = document.getElementById("board");
+const pencilBtn = document.getElementById("pencilBtn");
+const rainbowBtn = document.getElementById("rainbowBtn");
+const eraserBtn = document.getElementById("eraserBtn");
 
-colors = [
-    "#FAF0D7",
-    "#FFD9C0",
-    "#8CC0DE",
-    "#CCEEBC"
-];
+colorPicker.oninput = (event) => setColor(event.target.value); 
+sizeSlider.onmousemove = (event) => updateBoardSizeValue(event.target.value);
+sizeSlider.onchange = (event) => changeBoardSize(event.target.value);
+
+let mouseDown = false;
+board.onmousedown = () => (mouseDown = true);
+board.onmouseup = () => (mouseDown = false);
+
+
+function updateBoardSizeValue(value){
+    boardSizeValue.innerHTML = `${value} <i class="fa fa-light fa-xmark"></i> ${value}`;
+}
+
+function changeBoardSize(value){
+    setSize(Number(value));
+    clearBoard();
+}
+
+function setColor(color){
+    currentColor = color;
+}
+
+function setMode(mode){
+    currentMode = mode;
+
+    switch(mode){
+        case "pencil":
+            pencilBtn.classList.add("active");
+            rainbowBtn.classList.remove("active");
+            eraserBtn.classList.remove("active");
+            break;
+        case "rainbow":
+            rainbowBtn.classList.add("active");
+            pencilBtn.classList.remove("active");
+            eraserBtn.classList.remove("active");
+            break;
+        case "eraser":
+            eraserBtn.classList.add("active");
+            rainbowBtn.classList.remove("active");
+            pencilBtn.classList.remove("active");
+    }
+
+}
+
+function setSize(size){
+    currentSize = size;
+}
+
+function clearBoard(){
+    board.innerHTML = "";
+    setupBoard(currentSize);
+}
+
+function colorPixel(event){
+    if(event.type==="mouseover" && mouseDown===false){
+        return;
+    } else {
+        switch(currentMode){
+            case "pencil":
+                event.target.style.backgroundColor = currentColor;
+                break;
+            case "eraser":
+                event.target.style.backgroundColor = "white";
+                break;
+            case "rainbow":
+                let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, "0").toUpperCase();
+                event.target.style.backgroundColor = randomColor;
+                break;
+        }
+    }
+}
+
 
 function setupBoard(size){
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -16,6 +94,8 @@ function setupBoard(size){
     for(let i=0; i<size*size; i++){
         const pixel = document.createElement("div");
         pixel.classList.add("pixel");
+        pixel.addEventListener("mouseover", colorPixel);
+        pixel.addEventListener("mousedown", colorPixel);
         board.appendChild(pixel)
     }
 }
@@ -23,3 +103,4 @@ function setupBoard(size){
 window.onload = () =>{
     setupBoard(DEFAULT_SIZE);
 }
+
